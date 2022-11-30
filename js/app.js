4ed48aec2d757f528b55e457aa4c7e8a3d1b1c67
henriquesouzadev <henriquesouzadev@gmail.com>
2022-11-29 22:52:33 -0300

    ajuste app

--- js/app.js
+++ js/app.js
@@ -8,39 +8,44 @@ const timeIconContainer = document.querySelector('[data-js="time-icon"]')
 let timeImg = document.querySelector('[data-js="time"]')
 
 const timeImgSrc = (isDayTime) => {
-   const dayPeriod = isDayTime ? 'day' : 'night'
-   return `./src/${dayPeriod}.svg`
+  const dayPeriod = isDayTime ? 'day' : 'night'
+  return `./src/${dayPeriod}.svg`
 }
 
 const showCityCard = () => {
-   if (cityCard.classList.contains('d-none')) {
-      cityCard.classList.remove('d-none')
-   }
+  if (cityCard.classList.contains('d-none')) {
+    cityCard.classList.remove('d-none')
+  }
 }
 
-const showCityWeatherInfo = async (inputValue) => {
-   const [{ Key, LocalizedName }] = await getCityData(inputValue)
-   const [{
-      WeatherText,
-      Temperature,
-      IsDayTime,
-      WeatherIcon
-   }] = await getCityWeather(Key)
-   const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg" />`
-   
-   timeImg.src = timeImgSrc(IsDayTime)
-   timeIconContainer.innerHTML = timeIcon
-   cityNameContainer.textContent = LocalizedName
-   cityWeatherContainer.textContent = WeatherText
-   cityTemperatureContainer.textContent = Temperature.Metric.Value
+const fetchCityWeatherInfo = async (cityName) => {
+  const [{ Key, LocalizedName }] = await getCityData(cityName)
+  const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] =
+    await getCityWeather(Key)
+
+  return { LocalizedName, WeatherText, Temperature, IsDayTime, WeatherIcon }
 }
 
-cityForm.addEventListener('submit', (event) => {
-   event.preventDefault()
-   
-   const inputValue = cityForm.city.value
-   
-   showCityCard()
-   showCityWeatherInfo(inputValue)
-   cityForm.reset()
-})
+const showCityWeatherInfo = async (cityName) => {
+  const { LocalizedName, WeatherText, Temperature, IsDayTime, WeatherIcon } =
+    await fetchCityWeatherInfo(cityName)
+  const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg" />`
+
+  timeImg.src = timeImgSrc(IsDayTime)
+  timeIconContainer.innerHTML = timeIcon
+  cityNameContainer.textContent = LocalizedName
+  cityWeatherContainer.textContent = WeatherText
+  cityTemperatureContainer.textContent = Temperature.Metric.Value
+}
+
+const handleCityForm = (event) => {
+  event.preventDefault()
+
+  const cityName = cityForm.city.value
+
+  showCityCard()
+  showCityWeatherInfo(cityName)
+  cityForm.reset()
+}
+
+cityForm.addEventListener('submit', handleCityForm)
